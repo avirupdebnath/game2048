2121056e1c8b4666e6945ffa5e928c1ed557e0c3
Avirup Debnath <avirup.debnath@gmail.com>
2019-09-07 13:54:10 +0530

    Add files via upload

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import gameEngine
 from flask import Flask,request,render_template,flash,redirect,url_for
 app = Flask(__name__)
+app.debug = False 
 app.secret_key="random string"
 size=0
 grid=[]
@@ -9,8 +10,15 @@ score=0
 allscores=[]
 gameGrids=[]
 
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = 0
+    return response
+
 @app.route("/")
-def gameController():
+def gameController(): 
     global size 
     global grid 
     global flag
@@ -20,20 +28,23 @@ def gameController():
     size = 4
     grid = gameEngine.initGame(size)
     allscores.append(score)
+    allscores.append(score)
     flag=0
     score=0 
+    allscores=[]
+    gameGrids=[]
     gameGrids= [list(map(list,grid))]
     gameGrids.append(list(map(list,grid)))
     return render_template('game.html',score=score,grid1=grid[0][0],grid2=grid[0][1],grid3=grid[0][2],grid4=grid[0][3],grid5=grid[1][0],grid6=grid[1][1],grid7=grid[1][2],grid8=grid[1][3],grid9=grid[2][0],grid10=grid[2][1],grid11=grid[2][2],grid12=grid[2][3],grid13=grid[3][0],grid14=grid[3][1],grid15=grid[3][2],grid16=grid[3][3])
 
 @app.route("/", methods = ['POST','GET'] ) 
 def gameControllerPost():
-
     global grid
     global size
     global flag
     global score
     global gameGrids
+    global allscores
     status=1
     choice=str(request.form['control'])
     print(choice)
@@ -63,7 +74,19 @@ def gameControllerPost():
             grid=gameGrids.pop()
             score=allscores.pop()
         else:
-            print('cannot undo further!')       
+            print('cannot undo further!')  
+    elif(choice=='RESET'):
+        size = 4
+        grid = gameEngine.initGame(size)
+        score=0
+        allscores=[]
+        gameGrids=[]
+        allscores.append(score)
+        allscores.append(score)
+        flag=0 
+        gameGrids= [list(map(list,grid))]
+        gameGrids.append(list(map(list,grid)))
+        return render_template('game.html',score=score,grid1=grid[0][0],grid2=grid[0][1],grid3=grid[0][2],grid4=grid[0][3],grid5=grid[1][0],grid6=grid[1][1],grid7=grid[1][2],grid8=grid[1][3],grid9=grid[2][0],grid10=grid[2][1],grid11=grid[2][2],grid12=grid[2][3],grid13=grid[3][0],grid14=grid[3][1],grid15=grid[3][2],grid16=grid[3][3])
     else:
         print(100)
     allscores.append(score)
