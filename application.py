140b608f9c5ebf95cacde80c460e7a46c4f9f70b
Avirup Debnath <avirup.debnath@gmail.com>
2019-09-17 21:17:30 +0530

    Update application.py

--- application.py
+++ application.py
@@ -9,8 +9,6 @@ score=0
 allscores=[]
 gameGrids=[]
 
-
-
 @app.route("/")
 def gameController(): 
     global size 
@@ -81,8 +79,6 @@ def gameControllerPost():
         gameGrids= [list(map(list,grid))]
         gameGrids.append(list(map(list,grid)))
         return render_template('game.html',score=score,grid1=grid[0][0],grid2=grid[0][1],grid3=grid[0][2],grid4=grid[0][3],grid5=grid[1][0],grid6=grid[1][1],grid7=grid[1][2],grid8=grid[1][3],grid9=grid[2][0],grid10=grid[2][1],grid11=grid[2][2],grid12=grid[2][3],grid13=grid[3][0],grid14=grid[3][1],grid15=grid[3][2],grid16=grid[3][3])
-    else:
-        print(100)
     allscores.append(score)
     gameGrids.append(list(map(list,grid)))  
     if (status == 1):
