4cd6b5d39966edb4421c37310d092018be06820d
Avirup Debnath <avirup.debnath@gmail.com>
2019-09-19 10:55:37 +0530

    Update application.py

--- application.py
+++ application.py
@@ -40,7 +40,6 @@ def gameControllerPost():
     global allscores
     status=1
     choice=str(request.form['control'])
-    print(choice)
     if(choice=='UP'):
         grid,score = gameEngine.up(grid,size,score)
         grid,status = gameEngine.generate2(grid,size)
@@ -66,8 +65,6 @@ def gameControllerPost():
             allscores.pop()
             grid=gameGrids.pop()
             score=allscores.pop()
-        else:
-            print('cannot undo further!')  
     elif(choice=='RESET'):
         size = 4
         grid = gameEngine.initGame(size)
@@ -80,15 +77,11 @@ def gameControllerPost():
         gameGrids= [list(map(list,grid))]
         gameGrids.append(list(map(list,grid)))
         return render_template('game.html',score=score,grid1=grid[0][0],grid2=grid[0][1],grid3=grid[0][2],grid4=grid[0][3],grid5=grid[1][0],grid6=grid[1][1],grid7=grid[1][2],grid8=grid[1][3],grid9=grid[2][0],grid10=grid[2][1],grid11=grid[2][2],grid12=grid[2][3],grid13=grid[3][0],grid14=grid[3][1],grid15=grid[3][2],grid16=grid[3][3])
-    else:
-        print(100)
     allscores.append(score)
     gameGrids.append(list(map(list,grid)))  
     if (status == 1):
-        print(status)
         return render_template('game.html',score=score,grid1=grid[0][0],grid2=grid[0][1],grid3=grid[0][2],grid4=grid[0][3],grid5=grid[1][0],grid6=grid[1][1],grid7=grid[1][2],grid8=grid[1][3],grid9=grid[2][0],grid10=grid[2][1],grid11=grid[2][2],grid12=grid[2][3],grid13=grid[3][0],grid14=grid[3][1],grid15=grid[3][2],grid16=grid[3][3])
     else:
-        print(status)
         return render_template('gameover.html',score=score)
 if __name__ == "__main__":
     app.run(threaded=False)
